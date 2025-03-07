90d1e0936fab4198eb85a46f3eedccdb784702ec
cnriveraa <rivera.cn18@gmail.com>
2025-03-07 18:07:15 +0800

    Create prog01.py

---
+++ prog01.py
@@ -0,0 +1,7 @@
+#Prog01: Create a program that ask user to input 2 numbers. Print the bigger number.
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+if num1 > num2:
+    print("The bigger number is: ", num1)
+else:
+    print("The bigger number is: ", num2)
