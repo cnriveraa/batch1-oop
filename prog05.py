3f965844038d337f48225fe560115ab80d01c151
cnriveraa <rivera.cn18@gmail.com>
2025-03-07 18:13:36 +0800

    Create prog05.py

---
+++ prog05.py
@@ -0,0 +1,5 @@
+#Prog05: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers with the decimal point
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+quotient = num1 / num2
+print("The quotient of the two numbers is: ", quotient)
